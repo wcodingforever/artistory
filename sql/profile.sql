5de978256100390c9a393e1b0cb56537644d50d5
Alex <alexoh@wcoding.com>
2017-08-18 18:31:57 +0900

    Added the SQL table scripts.

---
+++ sql/profile.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS `profile`;
+CREATE TABLE `profile` (
+  `profileId` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `phone` int(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `socialMedia` varchar(255) DEFAULT NULL,
+  `interest` varchar(255) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `regTime` datetime NOT NULL,
+  PRIMARY KEY (`profileId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
