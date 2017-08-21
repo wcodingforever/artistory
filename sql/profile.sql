c4b14ee75cc61fa6563a30ce8e622743e8163acf
bibiliotique <saq2231@gmail.com>
2017-08-21 13:14:54 +0900

    Update profile.sql

--- sql/profile.sql
+++ sql/profile.sql
@@ -3,12 +3,12 @@ CREATE TABLE `profile` (
   `profileId` int(11) NOT NULL AUTO_INCREMENT,
   `firstName` varchar(50) NOT NULL,
   `lastName` varchar(50) NOT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
   `phone` int(30) NOT NULL,
   `email` varchar(50) NOT NULL,
   `socialMedia` varchar(255) DEFAULT NULL,
   `interest` varchar(255) DEFAULT NULL,
-  `city` varchar(50) DEFAULT NULL,
-  `country` varchar(50) DEFAULT NULL,
   `regTime` datetime NOT NULL,
   PRIMARY KEY (`profileId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
