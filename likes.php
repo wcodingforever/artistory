38f48319c59e204f48f3449ce42af1b6912c318c
tuatara <tuatara.guntheri@gmail.com>
2017-08-23 16:19:17 +0900

    rest API -'likes' and AJAX in 'buttons'

--- likes.php
+++ likes.php
@@ -1,9 +1,12 @@
 <?php 
 	
 	$dbConn = new PDO("mysql:hostname=;dbname=artistory","root","");
+	$errorTxt = "";
 
+//updating - saving likes into DB
 	$theID=($_REQUEST["id"]);
 	$action="";
+	if (isset($_REQUEST["id"])) {$theID = $_REQUEST["id"];}
 	if (isset($_REQUEST["action"])) { $action = $_REQUEST["action"]; }
 	//$user=($_REQUEST["username"]);
 
@@ -12,9 +15,12 @@
 		$dbRes = $dbStat->execute([":inid" => $theID]);
 	}
 
+// reading from the DB
 	$dbStat = $dbConn->prepare("SELECT likes, loves FROM fileuploadtable WHERE id=:inid");
 	$dbRes = $dbStat->execute([":inid" => $theID]);
 	while($row = $dbStat->fetch( PDO::FETCH_ASSOC)) {
-    print_r($row);}
+   /* print_r($row);}*/
+   echo ("RESULT likes: {$row['likes']".PHP_EOL);
+	}
 
 ?>
